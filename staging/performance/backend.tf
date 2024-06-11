56b554e009c25b107c42cd3920e3bb0d7cbddc2e
kelvin <admin@strategybase.io>
2024-06-10 20:28:12 -0500

    Added backend to my config

--- staging/performance/backend.tf
+++ staging/performance/backend.tf
@@ -1,12 +1,27 @@
+# terraform {
+#   backend "s3" {
+#     bucket   = "my-localstack-bucket"
+#     key      = "staging/perfomance/terraform.tfstate"
+#     region   = "us-east-1"
+#     # endpoint = "http://localhost:4566"
+#     endpoints                   = { s3 = "http://localhost:4566" }
+#     skip_credentials_validation = true
+#     skip_metadata_api_check     = true
+#     use_path_style            = true
+#   }
+# }
+
+
 terraform {
   backend "s3" {
-    bucket   = "my-localstack-bucket"
-    key      = "staging/perfomance/terraform.tfstate"
-    region   = "us-east-1"
-    # endpoint = "http://localhost:4566"
-    endpoints                   = { s3 = "http://localhost:4566" }
+    bucket                      = "pensa-test-bucket"
+    key                         = "staging/perfomance/terraform.tfstate"
+    region                      = "eu-west-1"
+    endpoints                   = { s3 = "https://minio.strategybase.io" }
+    skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    use_path_style            = true
+    use_path_style              = true
+    skip_requesting_account_id  = true
   }
 }
--- staging/performance/provider.tf
+++ staging/performance/provider.tf
@@ -1,9 +1,9 @@
-provider "aws" {
-  region                      = "us-east-1"
-  endpoints {
-    s3 = "http://localhost:4566"
-  }
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-}
+# provider "aws" {
+#   region                      = "us-east-1"
+#   endpoints {
+#     s3 = "http://localhost:4566"
+#   }
+#   skip_credentials_validation = true
+#   skip_metadata_api_check     = true
+#   skip_requesting_account_id  = true
+# }
