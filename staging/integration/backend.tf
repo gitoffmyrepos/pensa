8e97f858f9237c0007b1db8d3df70126f7540a4e
kelvin <admin@strategybase.io>
2024-06-10 19:57:09 -0500

    Added backend to my config

--- staging/integration/backend.tf
+++ staging/integration/backend.tf
@@ -3,7 +3,8 @@ terraform {
     bucket   = "my-localstack-bucket"
     key      = "staging/integration/terraform.tfstate"
     region   = "us-east-1"
-    endpoint = "http://localhost:4566"
+    # endpoint = "http://localhost:4566"
+    endpoints                   = { s3 = "http://localhost:4566" }
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     force_path_style            = true
