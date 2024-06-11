d4a0506cb1b5bec41bb0345ffdd69af3145822cb
kelvin <admin@strategybase.io>
2024-06-10 19:50:04 -0500

    Added backend to my config

---
+++ dev/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket   = "my-localstack-bucket"
+    key      = "dev/terraform.tfstate"
+    region   = "us-east-1"
+    endpoint = "http://localhost:4566"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
+}
